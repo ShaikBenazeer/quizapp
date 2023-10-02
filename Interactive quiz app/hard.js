effb39d775da99e53248a1bae55e369765ef9525
ShaikBenazeer <109638534+ShaikBenazeer@users.noreply.github.com>
2023-10-02 15:28:37 +0530

    Add files via upload

---
+++ Interactive quiz app/hard.js
@@ -0,0 +1,170 @@
+(function() {
+    var questions = [{
+      question: "What is the difference between HTML and HTML5?",
+      choices: ["HTML5 is the latest version of HTML and has no differences from previous versions", "A medium-speed downstream channeHTML5 introduces new elements, APIs, and improved support for multimedia, making it more powerful for web development compared to previous versions",
+       "HTML is used for client-side scripting, while HTML5 is used for server-side scripting."," HTML is for text content, and HTML5 is for multimedia content"],
+      correctAnswer: 1
+    }, {
+      question: "What is the purpose of the JavaScript prototype property in object-oriented programming, and how is it used to create inheritance?",
+      choices: ["The prototype property is used for naming objects in JavaScript", "The prototype property defines the visibility of object properties",
+       "The prototype property allows you to add properties and methods to an object's prototype, enabling inheritance in JavaScript", "The prototype property determines the type of data an object can store"],
+      correctAnswer: 2
+    }, {
+      question: "The CSS property font-size is used to control the _________________ of text.",
+      choices: ["size", "alignment","button","position" ],
+      correctAnswer: 0
+    }, {
+      question: "The CSS property ______-radius is used to control the roundness of an element's corners",
+      choices: ["border-curve", "corner-curve", "roundness", "border-radius"],
+      correctAnswer: 3
+    }, {
+      question: "What is the purpose of the HTML form element, and how is it used in web development?",
+      choices: ["The form element defines the footer of a webpage","The form element is used to create interactive user interfaces for collecting user input and submitting data to a server ",
+      "The form element specifies the document's character encoding","The form element is used to create hyperlinks between web pages."],
+      correctAnswer: 1
+    }];
+    
+    var questionCounter = 0; //Tracks question number
+    var selections = []; //Array containing user choices
+    var quiz = $('#quiz'); //Quiz div object
+    
+    // Display initial question
+    displayNext();
+    
+    // Click handler for the 'next' button
+    $('#next').on('click', function (e) {
+      e.preventDefault();
+      
+      // Suspend click listener during fade animation
+      if(quiz.is(':animated')) {        
+        return false;
+      }
+      choose();
+      
+      // If no user selection, progress is stopped
+      if (isNaN(selections[questionCounter])) {
+        alert('Please make a selection!');
+      } else {
+        questionCounter++;
+        displayNext();
+      }
+    });
+    
+    // Click handler for the 'prev' button
+    $('#prev').on('click', function (e) {
+      e.preventDefault();
+      
+      if(quiz.is(':animated')) {
+        return false;
+      }
+      choose();
+      questionCounter--;
+      displayNext();
+    });
+    
+    // Click handler for the 'Start Over' button
+    $('#start').on('click', function (e) {
+      e.preventDefault();
+      
+      if(quiz.is(':animated')) {
+        return false;
+      }
+      questionCounter = 0;
+      selections = [];
+      displayNext();
+      $('#start').hide();
+    });
+    
+    // Animates buttons on hover
+    $('.button').on('mouseenter', function () {
+      $(this).addClass('active');
+    });
+    $('.button').on('mouseleave', function () {
+      $(this).removeClass('active');
+    });
+    
+    // Creates and returns the div that contains the questions and 
+    // the answer selections
+    function createQuestionElement(index) {
+      var qElement = $('<div>', {
+        id: 'question'
+      });
+      
+      var header = $('<h2>Question ' + (index + 1) + ':</h2>');
+      qElement.append(header);
+      
+      var question = $('<p>').append(questions[index].question);
+      qElement.append(question);
+      
+      var radioButtons = createRadios(index);
+      qElement.append(radioButtons);
+      
+      return qElement;
+    }
+    
+    // Creates a list of the answer choices as radio inputs
+    function createRadios(index) {
+      var radioList = $('<ul>');
+      var item;
+      var input = '';
+      for (var i = 0; i < questions[index].choices.length; i++) {
+        item = $('<li>');
+        input = '<input type="radio" name="answer" value=' + i + ' />';
+        input += questions[index].choices[i];
+        item.append(input);
+        radioList.append(item);
+      }
+      return radioList;
+    }
+    
+    // Reads the user selection and pushes the value to an array
+    function choose() {
+      selections[questionCounter] = +$('input[name="answer"]:checked').val();
+    }
+    
+    // Displays next requested element
+    function displayNext() {
+      quiz.fadeOut(function() {
+        $('#question').remove();
+        
+        if(questionCounter < questions.length){
+          var nextQuestion = createQuestionElement(questionCounter);
+          quiz.append(nextQuestion).fadeIn();
+          if (!(isNaN(selections[questionCounter]))) {
+            $('input[value='+selections[questionCounter]+']').prop('checked', true);
+          }
+          
+          // Controls display of 'prev' button
+          if(questionCounter === 1){
+            $('#prev').show();
+          } else if(questionCounter === 0){
+            
+            $('#prev').hide();
+            $('#next').show();
+          }
+        }else {
+          var scoreElem = displayScore();
+          quiz.append(scoreElem).fadeIn();
+          $('#next').hide();
+          $('#prev').hide();
+          $('#start').show();
+        }
+      });
+    }
+    
+    // Computes score and returns a paragraph element to be displayed
+    function displayScore() {
+      var score = $('<p>',{id: 'question'});
+      
+      var numCorrect = 0;
+      for (var i = 0; i < selections.length; i++) {
+        if (selections[i] === questions[i].correctAnswer) {
+          numCorrect++;
+        }
+      }
+      
+      score.append('You got ' + numCorrect + ' questions out of ' +
+                   questions.length + ' right!!!');
+      return score;
+    }
+  })();
